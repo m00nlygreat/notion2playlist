9ad8a56bc1ba4bb4ad5bc3c251933be16cd13bbb
m00nlygreat <moonlygreat@gmail.com>
2022-09-04 23:44:25 +0900

    서버 부착

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 const { Client } = require("@notionhq/client")
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 dotenv.config()
 
-// Initializing a client
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 })
 
-console.log(process.env.NOTION_TOKEN);
 
+app.get('/', (req,res)=>{
+  (async () => {
+    const databaseId = '47e85059e4104489a236065f49007b7d';
+    const response = await notion.databases.query({
+      database_id: databaseId,
+  
+    });
+    // console.log(response);
+    const set_list = {songs:[]};
+    response.results.forEach(item => {
+      const _url = item.properties.url.url
+      console.log(_url)
+      set_list.songs = [ ... set_list.songs, {url:_url}]
+    })
+    res.json(set_list)
+  })();
+  
 
-(async () => {
-  const databaseId = '47e85059e4104489a236065f49007b7d';
-  const response = await notion.databases.query({
-    database_id: databaseId,
+})
 
-  });
-  // console.log(response);
-  response.results.forEach(item => {
-    console.log(item.properties.url.url)
-  })
-})();
+app.listen(port, () => {
+  console.log(`server is listening at localhost:${port}`);
+})
